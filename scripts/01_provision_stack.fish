de1e1cccd1eafd78b46f9a0fd4c1eaffbbb30d7a
Vaughn <vacoates@sei.cmu.edu>
2024-04-24 09:54:58 -0400

    rm docker-composes, update paths in neo4j setup

--- scripts/01_provision_stack.fish
+++ scripts/01_provision_stack.fish
@@ -1,12 +1,14 @@
 #!/usr/bin/fish
 
 # Neo4J
-mkdir -p neo4j_volumes/conf
-mkdir -p neo4j_volumes/data
-mkdir -p neo4j_volumes/import
-mkdir -p neo4j_volumes/logs
-mkdir -p neo4j_volumes/plugins
+mkdir -p ../conf/neo4j_volumes/conf
+mkdir -p ../conf/neo4j_volumes/data
+mkdir -p ../conf/neo4j_volumes/import
+mkdir -p ../conf/neo4j_volumes/logs
+mkdir -p ../conf/neo4j_volumes/plugins
 
-cp -r ../conf/neo4j_setup/plugins/* neo4j_volumes/plugins
-cp ../conf/neo4j_setup/conf/neo4j.conf neo4j_volumes/conf/neo4j.conf
-cp ../conf/neo4j_setup/imports/import* neo4j_volumes/import
+cp -r ../conf/neo4j_setup/plugins/* ../conf/neo4j_volumes/plugins
+cp ../conf/neo4j_setup/conf/neo4j_setup/neo4j.conf ..conf/neo4j_volumes/conf/neo4j.conf
+cp ../conf/neo4j_setup/imports/neo4j_setup/imports* ../conf/neo4j_volumes/import
+
+chown -R 7474:7474 ../conf/neo4j_volumes
