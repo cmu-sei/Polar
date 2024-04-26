d739afda5748fd521a270e346a1d668b2208ce57
David James Shepard <djshepard@sei.cmu.edu>
2024-04-26 15:01:39 -0400

    update project licensing

--- scripts/pim_process_activities/src/README.md
+++ scripts/pim_process_activities/src/README.md
@@ -1,3 +1,27 @@
+Polar (OSS)
+
+Copyright 2024 Carnegie Mellon University.
+
+NO WARRANTY. THIS CARNEGIE MELLON UNIVERSITY AND SOFTWARE ENGINEERING INSTITUTE
+MATERIAL IS FURNISHED ON AN "AS-IS" BASIS. CARNEGIE MELLON UNIVERSITY MAKES NO
+WARRANTIES OF ANY KIND, EITHER EXPRESSED OR IMPLIED, AS TO ANY MATTER
+INCLUDING, BUT NOT LIMITED TO, WARRANTY OF FITNESS FOR PURPOSE OR
+MERCHANTABILITY, EXCLUSIVITY, OR RESULTS OBTAINED FROM USE OF THE MATERIAL.
+CARNEGIE MELLON UNIVERSITY DOES NOT MAKE ANY WARRANTY OF ANY KIND WITH RESPECT
+TO FREEDOM FROM PATENT, TRADEMARK, OR COPYRIGHT INFRINGEMENT.
+
+Licensed under a MIT-style license, please see license.txt or contact
+permission@sei.cmu.edu for full terms.
+
+[DISTRIBUTION STATEMENT A] This material has been approved for public release
+and unlimited distribution.  Please see Copyright notice for non-US Government
+use and distribution.
+
+This Software includes and/or makes use of Third-Party Software each subject to
+its own license.
+
+DM24-0470
+
 We found limitations to import via Neo4J cypher alone. Overcoming automated
 import, at least in our case, meant creating a custom binary for this purpose.
 This small rust project will create an executable binary that loads certain
--- src/agents/gitlab/consume/src/groups_consumer.rs
+++ src/agents/gitlab/consume/src/groups_consumer.rs
@@ -1,17 +1,24 @@
 /*
-Polar (OSS)
+   Polar (OSS)
 
-Copyright 2024 Carnegie Mellon University.
+   Copyright 2024 Carnegie Mellon University.
 
-NO WARRANTY. THIS CARNEGIE MELLON UNIVERSITY AND SOFTWARE ENGINEERING INSTITUTE MATERIAL IS FURNISHED ON AN "AS-IS" BASIS. CARNEGIE MELLON UNIVERSITY MAKES NO WARRANTIES OF ANY KIND, EITHER EXPRESSED OR IMPLIED, AS TO ANY MATTER INCLUDING, BUT NOT LIMITED TO, WARRANTY OF FITNESS FOR PURPOSE OR MERCHANTABILITY, EXCLUSIVITY, OR RESULTS OBTAINED FROM USE OF THE MATERIAL. CARNEGIE MELLON UNIVERSITY DOES NOT MAKE ANY WARRANTY OF ANY KIND WITH RESPECT TO FREEDOM FROM PATENT, TRADEMARK, OR COPYRIGHT INFRINGEMENT.
+   NO WARRANTY. THIS CARNEGIE MELLON UNIVERSITY AND SOFTWARE ENGINEERING INSTITUTE MATERIAL IS
+   FURNISHED ON AN "AS-IS" BASIS. CARNEGIE MELLON UNIVERSITY MAKES NO WARRANTIES OF ANY KIND,
+   EITHER EXPRESSED OR IMPLIED, AS TO ANY MATTER INCLUDING, BUT NOT LIMITED TO, WARRANTY OF FITNESS
+   FOR PURPOSE OR MERCHANTABILITY, EXCLUSIVITY, OR RESULTS OBTAINED FROM USE OF THE MATERIAL.
+   CARNEGIE MELLON UNIVERSITY DOES NOT MAKE ANY WARRANTY OF ANY KIND WITH RESPECT TO FREEDOM FROM
+   PATENT, TRADEMARK, OR COPYRIGHT INFRINGEMENT.
 
-Licensed under a MIT-style license, please see license.txt or contact permission@sei.cmu.edu for full terms.
+   Licensed under a MIT-style license, please see license.txt or contact permission@sei.cmu.edu for
+   full terms.
 
-[DISTRIBUTION STATEMENT A] This material has been approved for public release and unlimited distribution.  Please see Copyright notice for non-US Government use and distribution.
+   [DISTRIBUTION STATEMENT A] This material has been approved for public release and unlimited
+   distribution.  Please see Copyright notice for non-US Government use and distribution.
 
-This Software includes and/or makes use of Third-Party Software each subject to its own license.
+   This Software includes and/or makes use of Third-Party Software each subject to its own license.
 
-DM24-0470
+   DM24-0470
 */
 
 use common::{GITLAB_EXCHANGE_STR, connect_to_rabbitmq, GROUPS_ROUTING_KEY, GROUPS_QUEUE_NAME};
