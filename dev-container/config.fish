4e879e52194e01dd3e305f42a36d7c0b5dbd4ee8
Maxfield Kassel <maxfieldkassel@gmail.com>
2024-07-11 11:42:06 -0400

    Updated phrases

--- dev-container/config.fish
+++ dev-container/config.fish
@@ -74,7 +74,7 @@ function fish_greeting --description="Displays the Fish logo and some other init
     lol_fig "Welcome to the Polar Shell."
     
     # Array of funny phrases
-    set phrases "Brace yourself for a flurry of brilliant code!" "Keep calm and code like an Eskimo!" "Our code is cooler than an ice cube in Antarctica!" "Get ready to chill and code!" "Let's make some code that's ice-olated in perfection!" "There are no polar bears, only coding bears!" "Next stop: Bug-free code!" "Where the only thing frozen is the bugs!" "It's time to break the ice and dive into development!" "Where every line of code is as cool as the Arctic!"
+    set phrases "Brace yourself for a flurry of brilliant code!" "Our code is cooler than an ice cube in Antarctica!" "Get ready to chill and code!" "Let's make some code that's ice-olated in perfection!" "There are no polar bears, only coding bears!" "Next stop: Bug-free code!" "Where the only thing frozen is the bugs!" "It's time to break the ice and dive into development!" "Where every line of code is as cool as the Arctic!" "Let's code like penguins on ice!" "Navigating the icy waters of code with ease!" "Our coding skills are as sharp as an icicle!" "Chill vibes, hot code!" "Frosty fingers, fiery code!" "Coding in a winter wonderland!" "Sliding into smooth code like a penguin!" "Our code is a polar express to success!" "Ice-cold focus, blazing fast code!" "From the tundra to triumph with our code!" "Arctic-level precision in every line!" "Coding through the polar vortex of bugs!" "Snow problem we can't solve with code!" "Where coding brilliance is as vast as the polar ice cap!" "Cool minds, warm hearts, perfect code!"
 
     # Select a random funny phrase
     set random_index (random 1 (count $phrases))
