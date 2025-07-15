f40c05377f8ead67746219b54ac300af5bda8e36
Vaughn Coates <vcaaron@rocketmail.com>
2025-07-14 21:19:38 -0400

    remove CI conditions

--- scripts/gitlab-ci.sh
+++ scripts/gitlab-ci.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Set CI var
-if [[ -n "${CI:-}" && "${CI}" == "true" ]]; then
-  # CI is set and true
-  echo "Running CI job: ${CI_JOB_NAME:-unknown}:${CI_JOB_ID:-unknown} on branch ${CI_COMMIT_BRANCH:-unknown}"
-else
-  echo "Running Polar CI tasks"
-fi
+# CI is set and true
+echo "Running CI job: ${CI_JOB_NAME:-unknown}:${CI_JOB_ID:-unknown} on branch ${CI_COMMIT_BRANCH:-unknown}"
 
 # git won't let nix operate if it doesn't think its safe.
 # TODO: We could elimiante this when we fully own the test runner's configuration.
@@ -33,9 +28,8 @@ done
 # Build core agent binaries
 nix build
 
-# Upload binaries to GitLab Package Registry if in CI
-if [[ -n "${CI:-}" && "${CI}" == "true" ]]; then
-  for file in result/bin/*; do
+# Upload binaries to GitLab Package Registry
+for file in result/bin/*; do
     if [[ -f "$file" ]]; then
       filename=$(basename "$file")
       curl --header "JOB-TOKEN: $CI_JOB_TOKEN" \
@@ -43,10 +37,7 @@ if [[ -n "${CI:-}" && "${CI}" == "true" ]]; then
            "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/packages/generic/polar/$CI_COMMIT_SHORT_SHA/$filename"
       echo "Uploaded binary: $filename"
     fi
-  done
-else
-  echo "Not in CI; skipping artifact upload."
-fi
+done
 
 # Build Docker images for all agent components
 echo Building agent images
@@ -83,27 +74,49 @@ function upload_image() {
 }
 
 # Log in and push to container registries if running in CI on the main branch
-if [[ -n "${CI:-}" && "${CI}" == "true" ]] && [ "$CI_COMMIT_REF_NAME" = "main" ]]; then
-  skopeo login --username "$CI_REGISTRY_USER" --password "$CI_REGISTRY_PASSWORD" "$CI_REGISTRY"
 
-  upload_image cassini "docker-archive:$(readlink -f cassini)" "docker://$CI_REGISTRY_IMAGE/cassini:$CI_COMMIT_SHORT_SHA"
-  upload_image gitlab-observer "docker-archive:$(readlink -f gitlab-observer)" "docker://$CI_REGISTRY_IMAGE/polar-gitlab-observer:$CI_COMMIT_SHORT_SHA"
-  upload_image gitlab-consumer "docker-archive:$(readlink -f gitlab-consumer)" "docker://$CI_REGISTRY_IMAGE/polar-gitlab-consumer:$CI_COMMIT_SHORT_SHA"
-  upload_image kube-observer "docker-archive:$(readlink -f kube-observer)" "docker://$CI_REGISTRY_IMAGE/polar-kube-observer:$CI_COMMIT_SHORT_SHA"
-  upload_image kube-consumer "docker-archive:$(readlink -f kube-consumer)" "docker://$CI_REGISTRY_IMAGE/polar-kube-consumer:$CI_COMMIT_SHORT_SHA"
-else
-    echo "Skipping image upload"
-fi
+skopeo login --username "$CI_REGISTRY_USER" --password "$CI_REGISTRY_PASSWORD" "$CI_REGISTRY"
+
+upload_image cassini "docker-archive:$(readlink -f cassini)" "docker://$CI_REGISTRY_IMAGE/cassini:$CI_COMMIT_SHORT_SHA"
+upload_image gitlab-observer "docker-archive:$(readlink -f gitlab-observer)" "docker://$CI_REGISTRY_IMAGE/polar-gitlab-observer:$CI_COMMIT_SHORT_SHA"
+upload_image gitlab-consumer "docker-archive:$(readlink -f gitlab-consumer)" "docker://$CI_REGISTRY_IMAGE/polar-gitlab-consumer:$CI_COMMIT_SHORT_SHA"
+upload_image kube-observer "docker-archive:$(readlink -f kube-observer)" "docker://$CI_REGISTRY_IMAGE/polar-kube-observer:$CI_COMMIT_SHORT_SHA"
+upload_image kube-consumer "docker-archive:$(readlink -f kube-consumer)" "docker://$CI_REGISTRY_IMAGE/polar-kube-consumer:$CI_COMMIT_SHORT_SHA"
 
 # Upload to Azure Container Registry (if configured)
-if [[ -z "${ACR_USERNAME:-}" && -n "${ACR_TOKEN:-}" && -n "${AZURE_REGISTRY:-}" ]]; then
-  echo "Uploading to Azure Container Registry..."
+echo "Uploading to Azure Container Registry..."
 
-  for image in cassini gitlab-observer gitlab-consumer kube-observer kube-consumer; do
+for image in cassini gitlab-observer gitlab-consumer kube-observer kube-consumer; do
     upload_image "$image" \
-      "docker-archive://$(readlink -f $image)" \
-      "docker://$AZURE_REGISTRY/$image:$CI_COMMIT_SHORT_SHA"
-  done
-else
-  echo "ACR credentials not configured; skipping Azure upload."
-fi
+    "docker-archive://$(readlink -f $image)" \
+    "docker://$AZURE_REGISTRY/$image:$CI_COMMIT_SHORT_SHA"
+done
+
+#TODO: Uncomment when we test the build job
+# Clone the chart repo
+# NOTE: A deploy key should be used used to enable read/write perms
+# An access token will do in the meantime
+# git clone --depth 1 "https://$POLAR_DEPLOY_USER:$CHART_REPO_TOKEN@gitlab.sandbox.labz.s-box.org/sei/polar-deploy.git"
+# # Generate helm charts and push them to a hosted repository
+# chmod +x ./scripts/render-manifests.sh
+# # delete old charts to make room for new configurations
+# rm -rf polar-deploy/manifests/
+
+# sh scripts/render-manifests.sh src/deploy/polar polar-deploy/manifests
+# cd polar-deploy
+
+# # Commit and push if there are changes
+
+# git config user.email $GITLAB_USER_EMAIL
+# git config user.name ci-job-$CI_JOB_NAME-$CI_JOB_ID
+# git add .
+
+# echo "Updated files:"
+# git diff --name-only --cached
+
+# echo "Writing metadata and pushing upstream..."
+# export TIMESTAMP=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
+# envsubst < ../scripts/metadata.yaml.tpl > metadata.yaml
+# git add metadata.yaml
+# git commit -m "Update manifests from source commit $CI_COMMIT_SHA"
+# git push origin "sandbox"
